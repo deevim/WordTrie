0109d6079dece29d82c510ef36bdedcd78e6c4b6
Manjula <manjula.ftc77@gmail.com>
2018-01-19 22:44:32 -0600

    Initial commit

---
+++ WordTrie/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordTrie.Trie;
+
+namespace WordTrie
+{
+    class Program
+    {
+
+        static string maxWord = "";
+        static string secodWord = "";
+        static int wordCount = 0;
+
+        static void Main(string[] args)
+        {
+            var root = new Node();
+
+            // add words to trie
+            using (var reader = new StreamReader("./wordlist.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if(String.IsNullOrWhiteSpace(line))
+                        continue;
+
+                    root.AddWord(line);
+                }
+            }
+
+            // retrieve longest & second longest words, and count of all words
+            TraverseTrie(root, false, "");
+
+            Console.WriteLine("Word Count : " + wordCount);
+            Console.WriteLine("Largest Composite Word : " + maxWord);
+            Console.WriteLine("Second Largest Composite Word : " + secodWord);
+            Console.ReadLine();
+
+        }
+
+        private static void TraverseTrie(Node root, bool isCompositeWord, string currentWord)
+        {
+            if(root == null)
+                return;
+
+            if (root.IsTerminal)
+            {
+                if (isCompositeWord)
+                {
+                    wordCount++;
+
+                    if (maxWord == secodWord)
+                    {
+                        maxWord = currentWord;
+                    }
+
+                    if (String.IsNullOrWhiteSpace(secodWord) && currentWord.Length < maxWord.Length)
+                    {
+                        secodWord = currentWord;
+                    }
+
+                    if (currentWord.Length > maxWord.Length)
+                    {
+                        secodWord = maxWord;
+                        maxWord = currentWord;
+                    }
+                }
+            }
+
+            
+
+            foreach (var nodeKey in root.GetChildKeys())
+            {
+                TraverseTrie(root.GetNode(nodeKey), isCompositeWord || root.IsTerminal, currentWord + root.Letter);
+            }
+        }
+    }
+}
---
+++ WordTrie/Trie/Node.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordTrie.Trie
+{
+    class Node
+    {
+        public Node()
+        {
+            IsTerminal = false;
+            childNodes = new Dictionary<char, Node>();
+        }
+        public char Letter { get; set; }
+        public bool IsTerminal { get; set; }
+        private Dictionary<char, Node> childNodes { get; set; }
+
+        public IEnumerable<char> GetChildKeys()
+        {
+            if(childNodes != null)
+                return childNodes.Keys;
+            
+            return new List<char>();
+        }
+
+        public Node GetNode(char key)
+        {
+            if (childNodes != null && childNodes.ContainsKey(key))
+                return childNodes[key];
+
+            return null;
+        }
+
+        public void AddWord(string word)
+        {
+            var root = this;
+            // todo : implement addition logic
+            AddWord(root, word, 0);
+        }
+
+        private void AddWord(Node root, string word, int idx)
+        {
+
+            /*if (idx == word.Length - 1)
+            {
+                root.IsTerminal = true;
+                return;
+            }*/
+
+            char key = word[idx];
+            if (!root.childNodes.ContainsKey(key))
+            {
+                var nextNode = new Node() {Letter = key};
+                if (idx == word.Length - 1)
+                {
+                    nextNode.IsTerminal = true;
+                }
+
+                root.childNodes.Add(key, nextNode);
+            }
+
+            if (idx == word.Length - 1)
+            {
+                //root.IsTerminal = true;
+                return;
+            }
+
+            // else
+            AddWord(root.childNodes[key], word, idx + 1);
+        }
+    }
+}
